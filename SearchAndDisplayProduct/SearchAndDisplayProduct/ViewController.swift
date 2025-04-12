2f0c6a587eb5d90da2bfd3230c0ceb3b0dd98cb2
Ricardo Hernández <ricardo.ohg96@gmail.com>
2025-04-12 01:29:32 -0600

    project created

---
+++ SearchAndDisplayProduct/SearchAndDisplayProduct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchAndDisplayProduct
+//
+//  Created by Ricardo Omar Hernández on 12/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SearchAndDisplayProduct/SearchAndDisplayProductTests/SearchAndDisplayProductTests.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAndDisplayProductTests.swift
+//  SearchAndDisplayProductTests
+//
+//  Created by Ricardo Omar Hernández on 12/04/25.
+//
+
+import Testing
+@testable import SearchAndDisplayProduct
+
+struct SearchAndDisplayProductTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
