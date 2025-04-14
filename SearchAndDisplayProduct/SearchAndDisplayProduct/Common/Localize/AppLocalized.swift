be404a22b7ce4ec13c0d4ef7028d380ccc40d6aa
Ricardo Hernández <ricardo.ohg96@gmail.com>
2025-04-14 17:37:15 -0600

    add appLocalized

---
+++ SearchAndDisplayProduct/SearchAndDisplayProduct/Common/Localize/AppLocalized.swift
@@ -0,0 +1,13 @@
+//
+//  AppLocalized.swift
+//  SearchAndDisplayProduct
+//
+//  Created by Ricardo Omar Hernández on 14/04/25.
+//
+
+struct AppLocalized {
+    static let searchBarPlaceholder = "Buscar en Mercado Libre"
+    static let defaultAlertTitle = "Alerta"
+    static let defaultOkButton = "Aceptar"
+    static let defaultCancelButton = "Cancelar"
+}
