4f5f6ea8ac07a97a9a32fea8681e8fe1ea046256
Ricardo Hernández <ricardo.ohg96@gmail.com>
2025-04-14 02:03:52 -0600

    add UIViewControllerExtension

--- SearchAndDisplayProduct/SearchAndDisplayProduct/Common/Extensions/UIViewControllerExtension.swift
+++ SearchAndDisplayProduct/SearchAndDisplayProduct/Common/Extensions/UIViewControllerExtension.swift
@@ -5,4 +5,13 @@
 //  Created by Ricardo Omar Hernández on 14/04/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func setupClearNavBar() {
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.barTintColor = .clear
+        navigationController?.navigationBar.isTranslucent = true
+    }
+}
