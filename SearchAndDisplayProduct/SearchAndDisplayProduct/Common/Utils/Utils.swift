76cd349be171ef2bbf1c963a33392710b3969e15
Ricardo Hernández <ricardo.ohg96@gmail.com>
2025-04-15 02:17:44 -0600

    function to downloadImage and get multiple images

--- SearchAndDisplayProduct/SearchAndDisplayProduct/Common/Utils/Utils.swift
+++ SearchAndDisplayProduct/SearchAndDisplayProduct/Common/Utils/Utils.swift
@@ -5,7 +5,8 @@
 //  Created by Ricardo Omar Hernández on 14/04/25.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
 
 struct Utils {
     static func formatPrice(_ value: Double) -> String {
@@ -14,5 +15,43 @@ struct Utils {
         numberFormatter.maximumFractionDigits = 0
         numberFormatter.minimumFractionDigits = 0
         return numberFormatter.string(from: NSNumber(value: value)) ?? "\(Int(value))"
+    }
+
+    static func getImagesFromUrlString(urlStrings: [String], completion: @escaping ([UIImage]) -> Void) {
+        let group = DispatchGroup()
+        var images = [UIImage]()
+        urlStrings.forEach { image in
+            group.enter()
+            downloadImage(with: image) {(image, _) in
+                guard let image = image else {
+                    group.leave()
+                    return
+                }
+                DispatchQueue.main.async {
+                    images.append(image)
+                    group.leave()
+                }
+            }
+        }
+
+        group.notify(queue: .main) {
+            completion(images)
+        }
+    }
+
+    static func downloadImage(`with` urlString: String, completion: @escaping (UIImage?, Error?) -> Void){
+        guard let url = URL.init(string: urlString) else {
+            return
+        }
+        let resource = KF.ImageResource(downloadURL: url)
+
+        KingfisherManager.shared.retrieveImage(with: resource, options: nil, progressBlock: nil) { result in
+            switch result {
+            case .success(let value):
+                completion(value.image, nil)
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
     }
 }
