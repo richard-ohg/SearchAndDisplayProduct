83ea6e4e3d4dafd3c9585b08f8320cbbd9a562ab
Ricardo Hernández <ricardo.ohg96@gmail.com>
2025-04-14 20:51:30 -0600

    custom collection view implemented

--- SearchAndDisplayProduct/SearchAndDisplayProduct/Common/Views/ProductListCollectionView.swift
+++ SearchAndDisplayProduct/SearchAndDisplayProduct/Common/Views/ProductListCollectionView.swift
@@ -5,4 +5,32 @@
 //  Created by Ricardo Omar Hernández on 14/04/25.
 //
 
-import Foundation
+import UIKit
+
+class ProductListCollectionView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        let layout = UICollectionViewFlowLayout()
+        let itemWidth = (ViewValues.widthScreen - ViewValues.doubleCollectionPadding - ViewValues.lineSpacingCollectionView) / ViewValues.multiplierTwo
+        layout.itemSize = CGSize(width: itemWidth, height: ViewValues.productItemHeight)
+        layout.minimumLineSpacing = ViewValues.lineSpacingCollectionView
+        layout.minimumInteritemSpacing = ViewValues.lineSpacingCollectionView
+        layout.sectionInset = UIEdgeInsets(
+            top: .zero,
+            left: ViewValues.normalCollectionPadding,
+            bottom: .zero,
+            right: ViewValues.normalCollectionPadding
+        )
+        super.init(frame: .zero, collectionViewLayout: layout)
+        config()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func config() {
+        register(ProductListViewCell.self, forCellWithReuseIdentifier: ProductListViewCell.reuseIdentifier)
+        backgroundColor = Colors.collectionBackgroundColor
+    }
+}
